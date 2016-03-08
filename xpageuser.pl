2689dba72d6f6f54fa96939499b1424a117016f9
Songmin Kim <songmink@hawaii.edu>
2016-03-07 16:23:24 -1000

    Modified extractors

--- xpageuser.pl
+++ xpageuser.pl
@@ -12,21 +12,19 @@ $password       = "wikidb";
 $dsn = "DBI:mysql:database=$name;host=$server;";
 $dbh = DBI->connect($dsn, $user, $password);
 
-$source = '/mnt/repo/eget/enwiki/enwiki-20160204-pages-meta-history1.xml';
+$source = 'pages_articles.xml';
 
 $pages = Parse::MediaWikiDump::Pages->new($source);
 print "Done parsing.\n";
 
 while(defined($page = $pages->page)) {
-    $c = $page->categories;
-    if (grep {/Mathermatics/} @$c) {  # all categories with the string "Mathermatics" anywhere in their text. 
-                                     # For exact match, use {$_ eq "Mathematics"}
 
         $id = $page->id;
         $title = $page->title;
-		#$text = $page->text;
+        $text = $page->text;
 
-        $dbh->do("insert into category (id, title) value ($id, $title)"); #details of SQL depend on the database setup
+        $dbh->do("insert ..."); #details of SQL depend on the database setup
+
         print "title '$title' id $id was inserted.\n";
     }
 }
